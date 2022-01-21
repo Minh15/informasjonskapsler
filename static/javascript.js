5397e99513ecb46919e533d6df0de3f1828553c8
Minh15 <minht@viken.no>
2022-01-22 00:54:47 +0100

    Lagt til .js og .css, og navbar

---
+++ static/javascript.js
@@ -0,0 +1,13 @@
+
+
+// Array for navbar. name: <"navn på siden">, href: <"link til siden">
+let navbar = [
+    {name:"A: InputToColor", href: "./inputToColor.html"},
+    {name:"B", href: ""},
+    {name:"C", href: ""},
+    {name:"D", href: ""},
+    {name:"E", href: ""},
+    {name:"F", href: ""},
+
+]
+
