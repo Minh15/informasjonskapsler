a98ce2eeab39f65172d3583f50e37ddb487b9c77
Minh15 <minht@viken.no>
2022-01-22 01:04:00 +0100

    index som hovedside

--- static/javascript.js
+++ static/javascript.js
@@ -2,12 +2,11 @@
 
 // Array for navbar. name: <"navn på siden">, href: <"link til siden">
 let navbar = [
-    {name:"A: InputToColor", href: "./inputToColor.html"},
-    {name:"B", href: ""},
+    {name:"A: Index", href: "./index.html"},
+    {name:"B: InputToColor", href: "./inputToColor.html"},
     {name:"C", href: ""},
     {name:"D", href: ""},
     {name:"E", href: ""},
     {name:"F", href: ""},
-
 ]
 
